3cb39dc110429796954e74fe521d12ff8ad4c0e1
Indra Basak <indra.basak@iovation.com>
2019-09-02 00:37:23 -0700

    More code smell cleanup.

--- edge-service/src/main/java/com/basaki/edge/security/Authenticator.java
+++ edge-service/src/main/java/com/basaki/edge/security/Authenticator.java
@@ -1,14 +1,12 @@
 package com.basaki.edge.security;
 
-import com.basaki.edge.exception.AuthenticationException;
-import com.basaki.edge.exception.BadCredentialsException;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 
 public interface Authenticator<C extends Credentials, A extends AuthenticationExtractor<C>> {
 
     A getExtractor();
 
-    C extract(ServerHttpRequest request) throws BadCredentialsException;
+    C extract(ServerHttpRequest request);
 
-    void authenticate(C credentials) throws AuthenticationException;
+    void authenticate(C credentials);
 }
